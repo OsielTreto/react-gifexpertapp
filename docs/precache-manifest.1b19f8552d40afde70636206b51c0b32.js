5a3479bff33891ce00990ca5f21880f108700b7a
Osiel Alejandro Treto Cedillo <osiel194treto@outlook.com>
2020-11-25 08:48:01 -0600

    Docs agregado

---
+++ docs/precache-manifest.1b19f8552d40afde70636206b51c0b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2403807a2876ae0b309abf7f7136c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df234802bb970667dfdd",
+    "url": "/static/css/main.2d4ab1f0.chunk.css"
+  },
+  {
+    "revision": "da4022958cb710cf147d",
+    "url": "/static/js/2.3e789d3f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.3e789d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df234802bb970667dfdd",
+    "url": "/static/js/main.344399a1.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
